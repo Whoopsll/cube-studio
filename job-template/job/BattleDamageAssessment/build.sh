76bf5a91d7adefa4c1e3dbf5a4bf89a03f941d1b
Whoopsll <63489202+Whoopsll@users.noreply.github.com>
2025-11-20 10:27:16 +0800

    Update build.sh

--- job-template/job/BattleDamageAssessment/build.sh
+++ job-template/job/BattleDamageAssessment/build.sh
@@ -6,7 +6,7 @@ set -ex
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR"
 
-IMAGE_NAME=ccr.ccs.tencentyun.com/cube-studio/battle-damage-assessment:20250101
+IMAGE_NAME=47.116.204.159:8089/cube/battle-damage-assessment:20251119
 
 docker build --network=host -t "$IMAGE_NAME" -f Dockerfile .
 docker push "$IMAGE_NAME"
